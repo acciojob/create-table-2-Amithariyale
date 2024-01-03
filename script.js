f1a4af608878dbeee657e81817befbd81fbfe905
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-03 13:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const table=document.getElementById('myTable');
+
 function createTable() {
     //Write your code here
+	const rn=prompt("Input number of rows");
+	const cn=prompt("Input number of columns");
+
+	for(let i=0;i<rn;i++){
+		const row=document.createElement('tr');
+
+		for(let j=0;j<cn;j++){
+			const col=document.createElement('td');
+			col.innerText=`Row-${i} Column-${j}`;
+			row.appendChild(col);
+		}
+		table.appendChild(row);
+	}
   
 }
